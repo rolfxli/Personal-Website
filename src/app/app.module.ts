fb1c4b9621e605f0f4a050d30187b57593d43f34
Rolf Li <rolf.x.li@gmail.com>
2019-08-07 20:18:33 -0400

    website framework set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainTextComponent } from './home/main-text/main-text.component';
+import { ConnectionsComponent } from './connections/connections.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainTextComponent,
+    ConnectionsComponent
   ],
   imports: [
     BrowserModule,
